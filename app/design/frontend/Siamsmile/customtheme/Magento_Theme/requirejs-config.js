5b55c4633c6c98ffc1f57701e85d3cbb91ba9ed2
unknown <pu_k5@hotmail.com>
2021-04-29 18:51:38 +0700

    first commit

---
+++ app/design/frontend/Siamsmile/customtheme/Magento_Theme/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    paths: {
+            'bootstrap':'Siamsmile/customtheme/js/bootstrap.bundle',
+    } ,
+    shim: {
+        'bootstrap': {
+            'deps': ['jquery']
+        }
+    }
+};
